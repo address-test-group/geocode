c55f717531be8558623fda47d8944a55f3a60ee9
Javier Ronda <f4ntom4s@gmail.com>
2018-02-02 13:08:12 -0300

    adding heroku to allowed hosts

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't2)tfx-mbgbu(#rh-%vybq-$m!oo5iqqdft$p5kcz0#6_gna9e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['geocode-demo.herokuapp.com]
+ALLOWED_HOSTS = ['geocode-demo.herokuapp.com']
 
 
 # Application definition
